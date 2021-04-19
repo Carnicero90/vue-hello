60864cbc5cfc6981ec1304de0ad8e30374621231
Calavera <calavera@localhost.localdomain>
2021-04-19 16:28:31 +0200

    sto fondamentalmente seguendo questo tutorial: https://www.vuemastery.com/courses/intro-to-vue-js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: '#root',
   data: {
     message: 'Bienvenue chez Vue!',
-    logo: 'https://vuejs.org/images/logo.png'
+    logo: 'https://vuejs.org/images/logo.png',
+    product: 'Socks',
+    image: 'img/blue.png'
   }
 })
