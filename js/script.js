42d74f0cc377241d887c70ca4a3a35f572d57324
Calavera <calavera@localhost.localdomain>
2021-04-19 16:42:52 +0200

    aggiunta lista dettagli prodotto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var app = new Vue({
     message: 'Bienvenue chez Vue!',
     logo: 'https://vuejs.org/images/logo.png',
     product: 'Socks',
-    image: 'img/blue.png'
+    image: 'img/blue.png',
+    inStock: true,
+    details: ["80% cotton", '20% crap', 'gender-neutral (sic!)']
   }
 })
