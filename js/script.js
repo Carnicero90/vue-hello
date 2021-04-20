42fd2058cf1e5173ba895562b31c6886745eb9bb
Calavera <calavera@localhost.localdomain>
2021-04-20 16:33:27 +0200

    avanti un poco nel tutorial, da riordinare

--- js/script.js
+++ js/script.js
@@ -4,8 +4,27 @@ var app = new Vue({
     message: 'Bienvenue chez Vue!',
     logo: 'https://vuejs.org/images/logo.png',
     product: 'Socks',
-    image: 'img/blue.png',
-    inStock: true,
-    details: ["80% cotton", '20% crap', 'gender-neutral (sic!)']
+    selectedVariant: 0,
+    cart: 0,
+    details: ["80% cotton", '20% crap', 'gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        image: 'img/green.png',
+        inStock: true
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        image: 'img/blue.png',
+        inStock: false
+      },
+    ]
+  },
+  methods: {
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
   }
 })
